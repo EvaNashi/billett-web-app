c46a25b88a85c3e8c32c700ac9fe3d43cc235722
EvaN7919 <nashi.eva@gmail.com>
2024-10-25 13:24:23 +0200

    Initial commit

---
+++ src/main/java/com/example/billettwebapp/BillettWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.billettwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillettWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BillettWebAppApplication.class, args);
+    }
+
+}
